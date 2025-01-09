2ae588f23757544bdd0081972373da087ebfa906
Bocoptor <qad14@hotmail.com>
2025-01-09 17:28:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bienvenue sur mon application web !"
+    return "Bienvenue sur mon applicatrion web !"
+            "Il s'agit d'un test pour le stage 2025 Alvarez Diaz Quentin"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
